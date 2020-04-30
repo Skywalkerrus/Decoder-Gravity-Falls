494ea497f8c0a5ac959d3da71246642cf74cdf5b
Skywalkerrus <i2010.s@yandex.ru>
2020-04-30 21:53:34 +0300

    Ready programm.

---
+++ app/src/main/java/com/example/decodergravityfalls/MainActivity.java
@@ -0,0 +1,221 @@
+package com.example.decodergravityfalls;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button b2 = findViewById(R.id.button2);
+        final Button b3 = findViewById(R.id.button3);
+        final Button b4 = findViewById(R.id.button4);
+        final Button b5 = findViewById(R.id.button5);
+        final Button b6 = findViewById(R.id.button6);
+        final Button b7 = findViewById(R.id.button7);
+        final Button b8 = findViewById(R.id.button8);
+        final Button b9 = findViewById(R.id.button9);
+        final Button b10 = findViewById(R.id.button10);
+        final Button b11 = findViewById(R.id.button11);
+        final Button b12 = findViewById(R.id.button12);
+        final Button b13 = findViewById(R.id.button13);
+        final Button b14 = findViewById(R.id.button14);
+        final Button b15 = findViewById(R.id.button15);
+        final Button b16 = findViewById(R.id.button16);
+        final Button b17 = findViewById(R.id.button17);
+        final Button b18 = findViewById(R.id.button18);
+        final Button b19 = findViewById(R.id.button19);
+        final Button b20 = findViewById(R.id.button20);
+        final Button b21 = findViewById(R.id.button21);
+        final Button b22 = findViewById(R.id.button22);
+        final Button b23 = findViewById(R.id.button23);
+        final Button b24 = findViewById(R.id.button24);
+        final Button b25 = findViewById(R.id.button25);
+        final Button b26 = findViewById(R.id.button26);
+        final Button b27 = findViewById(R.id.button27);
+        final Button b28 = findViewById(R.id.button28);
+        //final Button b29 = findViewById(R.id.button2);
+        final EditText tt = findViewById(R.id.editText);
+        showToast("Click on EYE for set 'space'! \n Click on picture and see what will happen!");
+        b28.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append(" ");
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("D");
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("E");
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("F");
+            }
+        });
+        b5.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("G");
+            }
+        });
+        b6.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("H");
+            }
+        });
+        b7.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("I");
+            }
+        });
+        b8.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("J");
+            }
+        });
+        b9.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("K");
+            }
+        });
+        b10.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("L");
+            }
+        });
+        b11.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("M");
+            }
+        });
+        b12.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("N");
+            }
+        });
+        b13.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("O");
+            }
+        });
+        b14.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("P");
+            }
+        });
+        b15.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("Q");
+            }
+        });
+        b16.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("R");
+            }
+        });
+        b17.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("S");
+            }
+        });
+        b18.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("T");
+            }
+        });
+        b19.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("U");
+            }
+        });
+        b20.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("V");
+            }
+        });
+        b21.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("W");
+            }
+        });
+        b22.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("X");
+            }
+        });
+        b23.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("Y");
+            }
+        });
+        b24.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("Z");
+            }
+        });
+        b25.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("B");
+            }
+        });
+        b26.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("C");
+            }
+        });
+        b27.setOnClickListener(new View.OnClickListener() { // кнопка перехода на "Главную" активность
+            @Override
+            public void onClick(View v) {
+                tt.append("A");
+            }
+        });
+    }
+    public void showToast(String mess) {
+        // Метод отображение тоста(всплывающего уведомления)
+        Toast toast = Toast.makeText(getApplicationContext(),
+                mess,
+                Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+    }
+}
